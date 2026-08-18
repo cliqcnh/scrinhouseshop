1d87ed4194c630a04419fa31fa7cc0d1020a3290
Emmanuel Nii Otoo Ribeiro <niioto4@gmmail.com>
2026-08-18 10:26:28 +0000

    config: disable image optimization to resolve Vercel Hobby plan limit

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,7 @@ const supabaseHostname = process.env.NEXT_PUBLIC_SUPABASE_URL
 
 const nextConfig: NextConfig = {
   images: {
+    unoptimized: true,
     remotePatterns: [
       { protocol: "https", hostname: "picsum.photos" },
       ...(supabaseHostname
